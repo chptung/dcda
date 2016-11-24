9070324d57ba7d2de1d492ba84a43e02a1d5bfc6
Chris Tung <chptung@gmail.com>
2016-11-24 13:09:19 -0800

    updated header and copy

--- DCDAC/src/App.js
+++ DCDAC/src/App.js
@@ -52,9 +52,11 @@ class App extends Component {
           <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
               <div className="container">
                   <div className="navbar-header">
+                      <div>
                       <a className="navbar-brand page-scroll" href="#page-top">
-                          <i className="fa fa-play-circle"></i> Davis College Democrats <span className="light">Alumni Club</span>
+                          Davis Alumni Democrats
                       </a>
+                      </div>
                       <div className="navbar-items">
                           <ul className="nav navbar-nav">
                               {/*  Hidden li included to remove active class from about link when scrolled up past about section */}
@@ -63,6 +65,9 @@ class App extends Component {
                               </li>
                               <li className="nav-item">
                                   <a className="page-scroll" href="#join">Join</a>
+                              </li>
+                              <li className="nav-item">
+                                  <a className="page-scroll" href="#contact">Contact</a>
                               </li>
                           </ul>
                       </div>
@@ -77,10 +82,10 @@ class App extends Component {
                   <div className="container">
                       <div className="row">
                           <div className="col-md-8 col-md-offset-2">
-                              <div className="brand-heading">Join The Davis College Democrats Alumni Club</div>
-                              <div className="intro-text">Contribute to the success of fellow democrats.<br />
-                              Help shape our political landscape<br />
-                              Make a difference
+                              <div className="brand-heading">Join The Davis Alumni Democrats</div>
+                              <div className="intro-text">Help fellow democrats succeed<br />
+                              Shape our political landscape<br />
+                              Make a REAL difference
                               </div>
                               <div className="arrow bounce">
                               </div>
@@ -94,54 +99,40 @@ class App extends Component {
           <section id="about" className="container content-section text-center">
               <div className="row">
                   <div className="col-lg-8 col-lg-offset-2">
-                      <h2>About Grayscale</h2>
-                      <p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-                      <p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-                      <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
-                  </div>
+                      <h2>Why The World Needs Davis Alumni Democrats</h2>
+                      Plans to privatize Medicare, roll back climate change deals, and restore the House Un-American Activities Committee announced by the Trump transition team and his enablers have made it even clearer that our future is in peril.<br /><br />
+                      However, Davis Alumni Democrats is here to join the growing resistance of like-minded people who oppose the policies that will hollow out the middle class in favor of lining the pockets of the wealthiest Americans, undermine our national security, and roll back hard won civil rights gains.<br /><br />
+                    </div>
               </div>
           </section>
 
           {/* Download Section */}
-          <section id="download" className="content-section text-center">
+          <section id="join" className="content-section text-center">
               <div className="download-section">
                   <div className="container">
                       <div className="col-lg-8 col-lg-offset-2">
-                          <h2>Download Grayscale</h2>
-                          <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
-                          <a href="http://startbootstrap.com/template-overviews/grayscale/" className="btn btn-default btn-lg">Visit Download Page</a>
+                          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeAocb_tY4XmOjazcvqsDTTSrCVT8N6a77mSiivznIXY2youQ/viewform?embedded=true" width="100%" height="2200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
                       </div>
                   </div>
               </div>
           </section>
 
           {/* Contact Section */}
-          <section id="join" className="container content-section text-center">
+          <section id="contact" className="container content-section text-center">
               <div className="row">
                   <div className="col-lg-8 col-lg-offset-2">
-                      <h2>Contact Start Bootstrap</h2>
-                      <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-                      <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+                      <h2>Contact Us</h2>
+                      <p>Feel free to email us!</p>
+                      <p><a href="mailto:dcdalumniclub@yahoo.com">dcdalumniclub@yahoo.com</a>
                       </p>
-                      <ul className="list-inline banner-social-buttons">
-                          <li>
-                              <a href="https://twitter.com/SBootstrap" className="btn btn-default btn-lg"><i className="fa fa-twitter fa-fw"></i> <span className="network-name">Twitter</span></a>
-                          </li>
-                          <li>
-                              <a href="https://github.com/IronSummitMedia/startbootstrap" className="btn btn-default btn-lg"><i className="fa fa-github fa-fw"></i> <span className="network-name">Github</span></a>
-                          </li>
-                          <li>
-                              <a href="https://plus.google.com/+Startbootstrap/posts" className="btn btn-default btn-lg"><i className="fa fa-google-plus fa-fw"></i> <span className="network-name">Google+</span></a>
-                          </li>
-                      </ul>
                   </div>
               </div>
           </section>
 
           {/*  Footer */}
           <footer>
-              <div className="container text-center">
-                  <p>Copyright &copy; Your Website 2016</p>
+              <div className="container text-center copyright">
+                  Copyright &copy; Davis Alumni Democrats 2016
               </div>
           </footer>
       </div>
